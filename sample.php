476d076444005fd719f10050b9a4832ba30afcba
RushabhPipada <rushabhpipada@gmail.com>
2016-10-19 17:03:54 -0400

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,8 +1,8 @@
 <?php
     header("content-type: text/xml");
-	$headache 	= array("OTC Crocin may help! Need doctor? Reply <headachedoctor>");
+	$headache = array("OTC Crocin may help! Need doctor? Reply <headachedoctor>");
 	$stomachache = array("OTC Tylenol may help! Need doctor? Reply <stomachachedoctor>");
-	$cough 	= array("OTC Robitussin Cough may help! Need doctor? Reply <coughdoctor>");
+	$cough = array("OTC Robitussin Cough may help! Need doctor? Reply <coughdoctor>");
 	$cold = array("OTC Aspirin may help! Need doctor? Reply <colddoctor>");
 	$vomiting = array("OTC Pepto-Bismol may help! Need doctor? Reply <vomitingdoctor>");
 	$headachedoctor = array("Dr. Lorem Ipsum1 - Here is the address! Call at +1 xxx xxx xxxx");
@@ -12,9 +12,6 @@
 	$vomitingdoctor = array("Dr. Lorem Ipsum5 - Here is the address! Call at +1 xxx xxx xxxx");
 	
 	
-	$headacheanswer   = 2;
-	$stomachacheanswer = 2;
-	$coughanswer   = 2;
     $quiz = array(
 	    "headache" => $headache,	
 	    "stomachache" => $stomachache,	
@@ -27,19 +24,11 @@
 	    "colddoctor" => $colddoctor,
 	    "vomiting" => $vomitingdoctor
 	);
-	// $to 	= $_REQUEST['to'];
-	// $from   = $_REQUEST['from'];
+	
 	$answer = $_REQUEST['Body'];
 	$reply  = array();
-	if (is_numeric($answer)) {
-		if($answer == 2){
-			$reply = 'Asrani';
-		}
-		else{
-			$reply = 'Wisrani';
-		}
-	}
-	else if(is_string($answer)){
+	
+	if(is_string($answer)){
 		array_push($reply, $quiz[$answer][0]);
 		foreach ($quiz[$answer][0] as $key => $value) {
 			array_push($reply, PHP_EOL);
